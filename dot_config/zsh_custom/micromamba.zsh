1f65a2a01a022bac1bfc77a52bcfcc6f77f1cf39
Josh Moles <2448063+jmoles@users.noreply.github.com>
2025-05-20 20:05:38 -0700

    Adding couple more zsh customs.

---
+++ dot_config/zsh_custom/micromamba.zsh
@@ -0,0 +1,14 @@
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'micromamba shell init' !!
+export MAMBA_EXE='/home/josh.moles/.local/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/home/josh.moles/micromamba';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
+
+eval "$(micromamba shell hook --shell zsh)"
